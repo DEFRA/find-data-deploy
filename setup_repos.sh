a648263a87d3fa5fd01f82e3167a5bb57b48aa8e
Ross Jones <ross@mailbolt.com>
2018-06-29 15:32:53 +0100

    Simple script to download the relevant repositories

---
+++ setup_repos.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+cd src
+git clone https://github.com/ckan/ckan.git
+git clone https://github.com/ckan/ckanext-dcat.git
+git clone https://github.com/ckan/ckanext-spatial.git
+git clone https://github.com/ckan/ckanext-harvest.git
+git clone git@gitlab.com:rossjones/ckanext-defra.git
+git clone https://github.com/jqnatividad/ckanext-officedocs.git
+
