e064a6df758278dd816e4c2344f5866124ce851b
Nick Ross <nick.ir.ross@gmail.com>
2019-02-01 12:10:03 +0000

    Enable stats collection via google analytics

--- compose/ckan/ckan-production-entrypoint.sh
+++ compose/ckan/ckan-production-entrypoint.sh
@@ -88,6 +88,9 @@ if [ ! -f /tmp/.initialized ]; then
     # Initialise the analytics db
     ckan-paster --plugin=ckanext-ga-report initdb -c /etc/ckan/production.ini
 
+    # Initialise the analytics db
+    ckan-paster --plugin=ckanext-ga-report initdb -c /etc/ckan/production.ini
+
     touch /tmp/.initialized
 fi
 
