8d97fa7e9b1e9e0563c6706792537dd5309db524
Nick Ross <nick.ir.ross@gmail.com>
2019-03-05 13:21:41 +0000

    Ensure postgis is initialised

--- compose/ckan/scripts/init_tests.sh
+++ compose/ckan/scripts/init_tests.sh
@@ -4,6 +4,9 @@ export PGPASSWORD=ckan
 # Do not pick up datastore db details from the environment
 unset CKAN_DATASTORE_READ_URL CKAN_DATASTORE_WRITE_URL
 
+psql -h db -p 5432 -U ckan_default -c "CREATE EXTENSION postgis;"
+psql -h db -p 5432 -U ckan_default -c "CREATE EXTENSION postgis_topology;;"
+
 # Create test databases
 psql -h db -p 5432 -U ckan_default -c "CREATE DATABASE ckan_test OWNER=ckan_default encoding='utf-8';"
 psql -h db -p 5432 -U ckan_default -c "CREATE DATABASE datastore_test OWNER=ckan_default encoding='utf-8';"
