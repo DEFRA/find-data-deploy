8a7c5c321c1a5e2fecbf35124edb68dcd2242509
Nick Ross <nick.ir.ross@gmail.com>
2019-03-27 11:56:46 +0000

    Migrate from datapusher to xloader

--- compose/postgresql/docker-entrypoint-initdb.d/00_create_datastore.sh
+++ compose/postgresql/docker-entrypoint-initdb.d/00_create_datastore.sh
@@ -6,3 +6,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE datastore OWNER ckan_default ENCODING 'utf-8';
     GRANT ALL PRIVILEGES ON DATABASE datastore TO ckan_default;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE_DATABASE xloader_jobs OWNER ckan_default ENCODING 'utf-8';
+    GRANT ALL PRIVILEGES ON DATABASE xloader_jobs TO ckan_default;
+EOSQL
