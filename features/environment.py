0deb63d43f1c2ae739dd1b49510c1ad7d60a1dd5
Nick Ross <nick.ir.ross@gmail.com>
2019-04-26 10:46:09 +0100

    Set up sauce labs for feature tests

--- features/environment.py
+++ features/environment.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium import webdriver
 
 from webdriver_manager.chrome import ChromeDriverManager
@@ -14,10 +16,21 @@ def before_feature(context, feature):
     }
 
     context.base_url = 'http://localhost:5000'
-    context.driver = webdriver.Chrome(
-        ChromeDriverManager().install(),
-        desired_capabilities=caps
-    )
+    context.base_url = 'http://ckan'
+    if 'TRAVIS' in os.environ:
+        username = os.environ['SAUCE_USERNAME']
+        access_key = os.environ['SAUCE_ACCESS_KEY']
+        caps['tunnel-identifier'] = os.environ['TRAVIS_JOB_NUMBER']
+        hub_url = '%s:%s@localhost:5000' % (username, access_key)
+        context.driver = webdriver.Remote(
+            desired_capabilities=caps,
+            command_executor="http://%s/wd/hub" % hub_url
+        )
+    else:
+        context.driver = webdriver.Chrome(
+            ChromeDriverManager().install(),
+            desired_capabilities=caps
+        )
 
 
 def after_feature(context, feature):
