d629bef1b05c771780f4b49219037e13026fcf2f
Nick Ross <nick.ir.ross@gmail.com>
2019-04-26 16:27:44 +0100

    Update travis build scripts to init ckan

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -6,6 +6,13 @@ echo "This is travis-build.bash..."
 echo "Installing test requirements"
 pip install -r requirements/test.txt
 
-docker-compose -f test.yml up -d
+echo "Building containers"
+docker-compose -f test.yml build
+
+echo "Creating test data"
+docker-compose -f test.yml run --rm test_selenium /usr/lib/ckan/venv/bin/ckan-paster --plugin=ckan create-test-data
+
+echo "Generating reports"
+docker-compose -f test.yml run --rm test_selenium /usr/lib/ckan/venv/bin/paster --plugin=ckanext-defrareports check_broken_resources
 
 echo "travis-build.bash is done."
