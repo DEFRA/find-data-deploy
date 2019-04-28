05af2152ab50835b08a911d58a303ef6ea690adb
Nick Ross <nick.ir.ross@gmail.com>
2019-04-28 23:36:07 +0100

    Move up command to run script

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "Bringing containers up"
+docker-compose -f test.yml up -d
+
 echo "Running feature tests"
 docker-compose -f test.yml run --rm test_selenium /deploy/compose/selenium/run_tests.sh; echo $?
