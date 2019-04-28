7e6facf1bb974ddfe3e3fda053ce070479ce7898
Nick Ross <nick.ir.ross@gmail.com>
2019-04-28 22:34:47 +0100

    Defer test data generation

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -9,10 +9,10 @@ pip install -r requirements/test.txt
 echo "Building containers"
 docker-compose -f test.yml build
 
-echo "Creating test data"
-docker-compose -f test.yml run --rm test_ckan /usr/lib/ckan/venv/bin/paster --plugin=ckan create-test-data
-
-echo "Generating reports"
-docker-compose -f test.yml run --rm test_ckan /usr/lib/ckan/venv/bin/paster --plugin=ckanext-report report generate -c /etc/ckan/test.ini
+#echo "Creating test data"
+#docker-compose -f test.yml run --rm test_ckan /usr/lib/ckan/venv/bin/paster --plugin=ckan create-test-data
+#
+#echo "Generating reports"
+#docker-compose -f test.yml run --rm test_ckan /usr/lib/ckan/venv/bin/paster --plugin=ckanext-report report generate -c /etc/ckan/test.ini
 
 echo "travis-build.bash is done."
