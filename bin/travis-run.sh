9ab955b7d7149c19c0708a01c9ac08ceb2ebb792
Nick Ross <nick.ir.ross@gmail.com>
2019-04-29 11:10:11 +0100

    Clean up travis conf and ensure fixtures are loaded

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e
 
-#echo "Bringing containers up"
-#docker-compose -f test.yml up -d
-#
-#sleep 5
-
 echo "Running feature tests"
 docker-compose -f test.yml run --rm test_selenium /deploy/compose/selenium/run_tests.sh; echo $?
--- compose/ckan/entrypoints/test-entrypoint.sh
+++ compose/ckan/entrypoints/test-entrypoint.sh
@@ -47,25 +47,6 @@ fi
 
 set_environment
 
-#function postgres_ready(){
-#/usr/lib/ckan/venv/bin/python << END
-#import sys
-#import psycopg2
-#try:
-#    conn = psycopg2.connect(dbname="$CKAN_DB_NAME", user="$CKAN_DB_USER", password="$POSTGRES_PASSWORD", host="$DB_HOST")
-#except psycopg2.OperationalError:
-#    sys.exit(-1)
-#sys.exit(0)
-#END
-#}
-#
-#until postgres_ready; do
-#  >&2 echo "Postgres is unavailable - sleeping - $CKAN_DB_NAME - $CKAN_DB_USER - $POSTGRES_PASSWORD - $DB_HOST"
-#  sleep 1
-#done
-#
-#>&2 echo "Postgres is up - continuing..."
-
 # Create the db tables
 ckan-paster --plugin=ckan db init -c ${CKAN_INI}
 ckan-paster --plugin=ckanext-report report initdb -c ${CKAN_INI}
